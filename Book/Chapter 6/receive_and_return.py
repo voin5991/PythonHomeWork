8d72609a3306e7a2676c5fd362c5834c204419b1
voin5991 <anlreyn@gmail.com>
2023-03-28 00:28:10 +0500

    functions init

---
+++ Book/Chapter 6/receive_and_return.py
@@ -0,0 +1,22 @@
+# Принимай - возвращай
+# Демонстрирует параметры и возвращаемые значения
+def display(message):
+    print(message)
+def give_me_five():
+    five = 5
+    return five
+def ask_yes_no(question):
+    """
+    Задает вопрос с ответом 'да' или 'нет'.
+    """
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+# основная часть
+display("Вам сообщение.\n")
+number = give_me_five()
+print("Вот что возвратила функция give_me_five():", number)
+answer = ask_yes_no("\nПожалуйста, введите 'y' или 'n': ")
+print("Спасибо, что ввели", answer)
+input("\n\nНажмите Enter, чтобы выйти.")

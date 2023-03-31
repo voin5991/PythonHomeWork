54838200d56ba9a3b94b171c56b566e63515bd84
voin5991 <anlreyn@gmail.com>
2023-04-01 01:07:08 +0500

    def_guess finish

--- Book/Chapter 6/def_guess_my_number.py
+++ Book/Chapter 6/def_guess_my_number.py
@@ -6,38 +6,71 @@
 # или попало в точку.
 
 import random
-
+# set the initial values
 the_number = random.randint(1, 100)
 tries = 0
 
 def intro():
+    """
+    Показывает вступление
+    :return:
+    """
     print("\tДобро пожаловать в игру 'Отгадай число'!")
     print("\nЯ загадал натуральное число из диапозона от 1 до 100")
     print("Постарайтесь отгадать его за 5 попыток.\n")
 
-def ask_number()
-guess = int(input("Ваше предположение: "))
+def ask_number(question, low, high):
+    """
+    Просит ввести число из диапазона.
+    :param question:
+    :param low:
+    :param high:
+    :return:
+    """
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
 
+def guessing_loop():
+    """
+    Основная часть. Просит угадать число
+    :return:
+    """
+    global the_number, tries
+    guess = ask_number("Ваше предположение: ", 1, 100)
+    while guess != the_number:
+        if guess > the_number:
+            print("Меньше...")
+        else:
+            print("Больше...")
+        tries += 1
+        print("Вы использовали ", tries, " попытку(ки)")
+        guess = ask_number("Ваше предположение: ", 1, 100)
+        if tries == 5:
+            print('Достигнут лимит попыток.')
+            break
+    return guess
 
-
-while guess != the_number and tries != 5:
-    if guess > the_number:
-        print("Меньше...")
+def congratulation(answer):
+    """
+    Выводит на экран результат игры
+    :return:
+    """
+    global the_number, tries
+    if answer == the_number:
+        print("Вам удалось отгадать число! Это в самом деле ", the_number)
+        print("Вы затратили на отгадывание всего лишь ", tries, "попыток(ки)!\n")
     else:
-        print("Больше...")
-    tries += 1
-    print("Вы потратили ", tries, " попытку(ки)")
-    guess = int(input("Ваше предположение: "))
+        print("""
+        Вы исчерпали свой лимит попыток и не смогли отгадать число!
+        Очень жаль... Я до конца не сомневался в Ваших способностях.
+        """)
 
-if tries == 5:
-        print('Достигнут лимит попыток.')
-if guess == the_number:
-    print("Вам удалось отгадать число! Это в самом деле ", the_number)
-    print("Вы затратили на отгадывание всего лишь ", tries, "попыток(ки)!\n")
-else:
-    print("""
-    Вы исчерпали свой лимит попыток и не смогли отгадать число!
-    Очень жаль... Я до конца не сомневался в Ваших способностях.
-    """)
+def main():
+    intro()
+    answer = guessing_loop()
+    congratulation(answer)
 
+main()
 input("\n\nPress the enter key to exit.")

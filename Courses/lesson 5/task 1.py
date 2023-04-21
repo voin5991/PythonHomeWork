403a8240289d19bb6341856d7b345645a5a5ce5d
voin5991 <76397893+voin5991@users.noreply.github.com>
2023-04-21 14:42:53 +0500

    Lesson_5 tasks edit

--- Courses/lesson 5/task 1.py
+++ Courses/lesson 5/task 1.py
@@ -1,3 +1,32 @@
-# TODO: Создайте программу, которая состоит из функции, которая принимает три числа и возвращает их
-#   среднее арифметическое, и главного цикла, спрашивающего у пользователя числа и вычисляющего их средние значения
-#   при помощи созданной функции
+def math_average(a, b, c):
+    average = (a + b + c) // 3
+    return average
+
+
+def main():
+    choice = None
+    a = None
+    b = None
+    c = None
+    average = None
+    while choice != "0":
+        print("""
+                    Меню:
+                0 - Выход
+                1 - Рассчёт
+        """)
+        choice = input("Ваш выбор: ")
+        if choice == "0":
+            print("До свидания!")
+        elif choice == "1":
+            a = int(input("Введите первое число: "))
+            b = int(input("Введите второе число: "))
+            c = int(input("Ввеедите третье число: "))
+            average = math_average(a, b, c)
+            print("\nСреднее арифметическое чисел", a, b, c, "равно", average)
+        else:
+            print("Извините, в меню нет пункта", choice)
+
+
+main()
+input("\n\nНажмите Enter, чтобы выйти.")
--- Courses/lesson 5/task 4.py
+++ Courses/lesson 5/task 4.py
@@ -1,4 +1,64 @@
-# TODO: Создайте программу-калькулятор, которая поддерживает четыре операции: сложение, вычитание, умножение, деление.
-#  Все данные должны вводиться в цикле, пока пользователь не укажет, что хочет завершить выполнение программы.
-#  Каждая операция должна быть реализована в виде отдельной функции. Функция деления должна проверять данные на
-#  корректность и выдавать сообщение об ошибке в случае попытки деления на ноль.
+def addition(a, b):
+    result = a + b
+    return result
+
+
+def subtraction(a, b):
+    result = a - b
+    return result
+
+
+def multiplication(a, b):
+    result = a * b
+    return result
+
+
+def division(a, b):
+    if a != 0 and b != 0:
+        result = a / b
+        return result
+    else:
+        print("На 0 делить нельзя!")
+
+
+def main():
+    choice = None
+    a = None
+    b = None
+    while choice != "0":
+        print("""
+                        Меню:
+                    0 - Выход
+                    1 - Сложение
+                    2 - Вычитание
+                    3 - Умножение
+                    4 - Деление
+            """)
+        choice = input("Ваш выбор: ")
+        if choice == "0":
+            print("До свидания!")
+        elif choice == "1":
+            a = int(input("Введите первое число: "))
+            b = int(input("Введите второе число: "))
+            result = addition(a, b)
+            print(a, "+", b, "=", result)
+        elif choice == "2":
+            a = int(input("Введите первое число: "))
+            b = int(input("Введите второе число: "))
+            result = subtraction(a, b)
+            print(a, "-", b, "=", result)
+        elif choice == "3":
+            a = int(input("Введите первое число: "))
+            b = int(input("Введите второе число: "))
+            result = multiplication(a, b)
+            print(a, "*", b, "=", result)
+        elif choice == "4":
+            a = int(input("Введите первое число: "))
+            b = int(input("Введите второе число: "))
+            result = division(a, b)
+            print(a, "/", b, "=", result)
+        else:
+            print("Извините, в меню нет пункта", choice)
+
+main()
+input("\n\nНажмите Enter, чтобы выйти.")

aba8171f35a70d7b060912410f8eaab3b360e207
voin5991 <anlreyn@gmail.com>
2023-04-23 18:05:05 +0500

    lesson_6_tasks_init

--- Courses/lesson_6/task_2.py
+++ Courses/lesson_6/task_2.py
@@ -1,1 +1,15 @@
-# TODO: Создайте программу, которая проверяет, является ли палиндромом введённая фраза.
+def palindrome(word):
+    """
+    Определяет, является ли фраза палиндромом
+    :param word:
+    :return:
+    """
+    rev = ''.join(reversed(word))
+    if rev == word:
+        print("Это палиндром!")
+    else:
+        print("Это не палиндром!")
+
+
+string = input("Введите фразу для проверки: ")
+print(palindrome(string))

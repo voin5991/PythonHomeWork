2b136c2784614f535830360185a304c8aa6d6527
voin5991 <anlreyn@gmail.com>
2023-04-25 02:25:55 +0500

    PE_L1_task_2 complete

--- Courses/Python_Essential/Lesson_1/Tasks/task_2.py
+++ Courses/Python_Essential/Lesson_1/Tasks/task_2.py
@@ -1,3 +1,25 @@
-# TODO: Создайте класс, описывающий книгу. Он должен содержать информацию об авторе, названии, годе издания и жанре.
-#  Создайте несколько разных книг. Определите для него операции проверки на равенство и неравенство,
-#  методы __repr__ и __str__.
+class Book:
+    def __init__(self, author, book_name, year, genre):
+        self.author = author
+        self.book_name = book_name
+        self.year = year
+        self.genre = genre
+
+    def __eq__(self, other):
+        return self.author == other.author and self.book_name == other.book_name and self.year == other.year and \
+            self.genre == other.genre
+
+    def __repr__(self):
+        return "{}, {}, {!r}, {}".format(self.author, self.book_name, self.year, self.genre)
+
+    def __str__(self):
+        return "{}, {}, {!r}, {}".format(self.author, self.book_name, self.year, self.genre)
+
+
+book_1 = Book("Жюль Верн", "Таинственный остров", 1873, "Робинзонада")
+book_2 = Book("Александр Дюма", "Граф Монте-Кристо", 1846, "Исторический роман")
+book_3 = Book("Майкл Доусон", "Программируем на Python", 2022, "Учебное пособие")
+
+print(book_1 == book_2)
+print(book_2 != book_1)
+print(book_3)

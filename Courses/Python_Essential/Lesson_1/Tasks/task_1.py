a3fa7b4bab0bc8fbcf5681135d0b389ba86c95bc
voin5991 <76397893+voin5991@users.noreply.github.com>
2023-05-09 10:27:30 +0500

    L1_t1_t4 init

--- Courses/Python_Essential/Lesson_1/Tasks/task_1.py
+++ Courses/Python_Essential/Lesson_1/Tasks/task_1.py
@@ -18,23 +18,24 @@ class Car(object):
             """.format(self.brand, self.model, self.body_type, self.production_year, self.price)
 
 
+car_1 = Car("Toyota", "Land Cruiser", "SUV", "2016", 6000000)
+car_2 = Car("Ford", "Focus", "Sedan", "2006", 475000)
+car_3 = Car("LADA", "Priora", "High back", "2011", 210000)
+car_4 = Car("Honda", "Accord", "Sedan", "2011", 1265000)
+
+
 class Car_showroom:
     def __init__(self, cars, choice):
-        car_1 = Car("Toyota", "Land Cruiser", "SUV", "2016", 6000000)
-        car_2 = Car("Ford", "Focus", "Sedan", "2006", 475000)
-        car_3 = Car("LADA", "Priora", "High back", "2011", 210000)
-        car_4 = Car("Honda", "Accord", "Sedan", "2011", 1265000)
-        self.cars = cars = [car_1, car_2, car_3, car_4]
+        cars = [car_1, car_2, car_3, car_4]
+        self.cars = cars
         self.choice = choice
 
-    def sell(self, choice):
+    def sell(self, choice, cars):
         """
         Функция продажи автомобиля
         :return:
         """
-        if self.choice in self.cars:
-            for i in self.cars:
-
-
-
-
+        if choice in cars:
+            del cars[choice]
+        else:
+            print("Wrong choice!")
--- Courses/Python_Essential/Lesson_1/Tasks/task_4.py
+++ Courses/Python_Essential/Lesson_1/Tasks/task_4.py
@@ -1,4 +1,15 @@
-# TODO: Используя ссылки в конце данного урока, ознакомьтесь с таким средством инкапсуляции как свойства.
-#  Ознакомьтесь с декоратором property в Python. Создайте класс, описывающий температуру и позволяющий задавать и
-#  получать температуру по шкале Цельсия и Фаренгейта, причём данные могут быть заданы в одной шкале,
-#  а получены в другой.
+class Temperature(object):
+    def __init__(self, celsius=0):
+        self.celsius = celsius
+
+    @property
+    def fahrenheit(self):
+        return self.celsius * 1.8 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = (value - 32) / 1.8
+
+    def __str__(self):
+        return 'Celsius: {:g}\nFahrenheit: {:g}\n'.format(self.celsius, self.fahrenheit)
+
